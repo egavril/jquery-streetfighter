b00f6bb9b5531281ec3152cd55e5b6f1a5f984df
Tyler van der Hoeven <hi@tyvdh.com>
2016-03-07 23:50:57 -0500

    updated app.js

--- js/app.js
+++ js/app.js
@@ -1,47 +1,62 @@
-$(document).ready(function() {
-	$('.ryu').mouseenter(function() {
-		$('.ryu-still').hide();
-		$('.ryu-ready').show();
-	})
-	.mouseleave(function() {
-		$('.ryu-ready').hide();
-		$('.ryu-still').show();
-	})
-	.mousedown(function() {
-		playHadouken();
-		$('.ryu-ready').hide();
-		$('.ryu-throwing').show();
-		$('.hadouken').finish().show().animate(
-			{'left': '1057px'},
-			500,
-			function() {
-				$(this).hide();
-				$(this).css('left', '557px');
-			}
-		);
-	})
-	.mouseup(function() {
-		$('.ryu-throwing').hide();
-		$('.ryu-ready').show();
-	});
+$(function() {
+  var ryu =       $('.ryu'),
+      still =     $('.ryu-still'),
+      ready =     $('.ryu-ready'),
+      throwing =  $('.ryu-throwing'),
+      hadouken =  $('.hadouken'),
+      cool =      $('.ryu-cool');
+
+  var triggers = {
+    'mouseenter': function() {
+      still.hide();
+      ready.show();
+    },
+    'mouseleave': function() {
+      ready.hide();
+      still.show();
+    },
+    'mousedown': function() {
+      playHadouken();
+      ready.hide();
+      throwing.show();
+      hadouken
+        .finish()
+        .show()
+        .animate({'left': '1057px'}, 500, function() {
+          $(this).hide().css('left', '557px');
+        });
+    },
+    'mouseup': function() {
+      throwing.hide();
+      ready.show();
+    }
+  }
+
+  ryu.on(triggers);
+
+  $(document).on({
+    'keydown': function(e) {
+      if (e.keyCode == 88) {
+        ryu.off(triggers);
+
+        still.hide();
+        ready.hide();
+        cool.show();
+      }
+    },
+    'keyup': function() {
+      still.show();
+      cool.hide();
+
+      ryu.on(triggers);
+    }
+  });
+
+  function playHadouken () {
+    var sound = $('#hadouken-sound')[0];
+
+    sound.volume = 0.5;
+    sound.load();
+    sound.play();
+  }
 });
-
-$(document).keydown(function(e) {
-	if (e.keyCode == 88) {
-		$('.ryu-still').hide();
-		$('.ryu-ready').hide();
-		$('.ryu-cool').show();
-	}
-})
-.keyup(function() {
-	$('.ryu-still').show();
-	$('.ryu-cool').hide();
-});
-
-
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
-}
-
