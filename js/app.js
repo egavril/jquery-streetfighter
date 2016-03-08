146e7b6ec90d38deaa44507922a29e9e4209e48c
egavril <e.gavrilyuk@gmail.com>
2016-03-07 18:59:54 -0800

    done...ish

--- js/app.js
+++ js/app.js
@@ -23,12 +23,25 @@ $(document).ready(function() {
 	.mouseup(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
-		// ryu back to ready position
 	});
 });
+
+$(document).keydown(function(e) {
+	if (e.keyCode == 88) {
+		$('.ryu-still').hide();
+		$('.ryu-ready').hide();
+		$('.ryu-cool').show();
+	}
+})
+.keyup(function() {
+	$('.ryu-still').show();
+	$('.ryu-cool').hide();
+});
+
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
+
